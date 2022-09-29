d1d716bfde9d41069e0a9ab2183857d0f21aebb4
AngelClase <angel.torijasanchezj@iesfm.onmicrosoft.com>
2022-09-29 13:13:34 +0200

    Actividad3

--- src/main/java/app/Actividad3.java
+++ src/main/java/app/Actividad3.java
@@ -6,6 +6,7 @@ import java.io.File;
 
 public class Actividad3 {
 
+    //Direccion de tu ordenador
     private static final String PATH = "/angel/Documentos";
 
     public static void main(String[] args) {
